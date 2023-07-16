4c663c043f45f5ad0c776fbcb7379b4d78831bc3
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-07-16 23:27:40 +0100

    moves tests to separate folder

--- src/components/tests/W12Answers.test.tsx
+++ src/components/tests/W12Answers.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Answers } from './W12Answers';
+import { Answers } from '../W12Answers';
 
 const answersExample = {
   speciesName : 'Dalek',
